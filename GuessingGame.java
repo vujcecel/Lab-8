d3a0b9b1c2a0306ff39d699d6bc08a15f5e5632e
vujcecel <vujcecel@mail.uc.edu>
2021-03-29 00:29:20 +0000

    Initial commit

---
+++ GuessingGame.java
@@ -0,0 +1,77 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+class GuessingGame implements ActionListener {
+  JFrame frame;
+  JTextField guessField;
+  JButton guessButton, playAgainButton;
+  JLabel promptLabel, directLabel, guessLabel;
+  Random random;
+  int randomNumber;
+
+  GuessingGame() {
+    random = new Random();
+    randomNumber = random.nextInt(100) + 1;
+
+    frame = new JFrame("Guessing Game");
+    frame.setLayout(new FlowLayout());
+    frame.setSize(240, 120);
+
+    guessField = new JTextField(10);
+    guessField.setActionCommand("myTF");
+    guessButton = new JButton("Guess");
+    playAgainButton = new JButton("Play Again");
+
+    guessField.addActionListener(this);
+    guessButton.addActionListener(this);
+    playAgainButton.addActionListener(this);
+
+    promptLabel = new JLabel("Enter your guess: ");
+    directLabel = new JLabel("");
+    guessLabel = new JLabel("");
+
+    frame.add(promptLabel);
+    frame.add(guessField);
+    frame.add(guessButton);
+    frame.add(directLabel);
+    frame.add(guessLabel);
+    frame.add(playAgainButton);
+
+    frame.setVisible(true);
+  }
+
+  public void actionPerformed(ActionEvent ae) {
+    if(ae.getActionCommand().equals("Guess"))
+      guessNumber();
+    else if(ae.getActionCommand().equals("Play Again"))
+      reset();
+    else if(ae.getActionCommand().equals("myTF"))
+      guessNumber();
+  }
+
+  public void guessNumber() {
+    frame.setSize(240, 140);
+
+    int guess = Integer.parseInt(guessField.getText());
+
+    if (guess > randomNumber)
+      directLabel.setText("Too high!");
+    else if (guess < randomNumber)
+      directLabel.setText("Too low!");
+    else
+      directLabel.setText("You got it!");
+
+    guessLabel.setText("Last guess was " + String.valueOf(guess));
+  }
+
+  public void reset() {
+    frame.setSize(240, 120);
+    randomNumber = random.nextInt(100) + 1;
+    guessField.setText("");
+    promptLabel = new JLabel("Enter your guess: ");
+    directLabel.setText("");
+    guessLabel.setText("");
+  }
+}
